d4bf682d0a34a3eac34065ac9faeeea9ede6efce
nukestarRoot <william.gurecky@gmail.com>
2015-11-09 07:14:23 -0600

    added gh_pages doc gen script

---
+++ doc/create_gh_pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Notes:
+# ------
+# Requires sphinx is install, auto html doc builder
+# Requires the following var is set in the sphinx makefile:
+# GH_PAGES_SOURCES = source Makefile
+
+git checkout gh-pages
+rm -rf build _sources _static
+touch .nojekyll
+git checkout master $(GH_PAGES_SOURCES)
+git reset HEAD
+make html
+mv -fv build/html/* ./
+rm -rf $(GH_PAGES_SOURCES) build
+git add -A
+git ci -m "Gen gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
