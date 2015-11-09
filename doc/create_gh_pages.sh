34343b4998a2c6534525dcb69b3b188ed3a5ea23
nukestarRoot <william.gurecky@gmail.com>
2015-11-09 07:48:29 -0600

    fixed create_gh_pages script

--- doc/create_gh_pages.sh
+++ doc/create_gh_pages.sh
@@ -6,13 +6,14 @@
 # Requires the following var is set in the sphinx makefile:
 # GH_PAGES_SOURCES = source Makefile
 
+GH_PAGES_SOURCES="source Makefile"
 git checkout gh-pages
-rm -rf build _sources _static
-touch .nojekyll
-git checkout master $(GH_PAGES_SOURCES)
+#rm -rf build _sources _static
+#touch .nojekyll
+git checkout master $GH_PAGES_SOURCES
 git reset HEAD
 make html
 mv -fv build/html/* ./
-rm -rf $(GH_PAGES_SOURCES) build
+rm -rf $GH_PAGES_SOURCES build
 git add -A
-git ci -m "Gen gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
+git commit -m "Gen gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
